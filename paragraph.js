45a89c8f70c3d17bd51f8895c2ff8b18096d89e3
JJ <jj@r2src.com>
2013-03-01 22:09:36 +0100

    copy-pasted great outlining algorithm

--- paragraph.js
+++ paragraph.js
@@ -1,3 +1,11 @@
+// poor man's jquery...
+function $(query) {
+    return document.querySelector(query);
+}
+
+function $$(query) {
+    return document.querySelectorAll(query);
+}
 var MutationObserver = window.MutationObserver || window.WebKitMutationObserver || window.MozMutationObserver;
 
 window.onload = function() {
@@ -18,246 +26,7 @@ window.onload = function() {
     $('#settings').addEventListener('click', make_toggler($('#settings_menu')));
 };
 
-// Extra array feature that is used by outline()
-Array.prototype.last = function() {
-    return this[this.length - 1];
-}
-
-/*
-                document
-document        +--section
-+--h1              +--h1
-+--p      ===>     +--p
-+--h2              +--section
-+--p                  +--h2
-                      +--p
-*/
-
-// Implementation of the outlining algorithm described in http://dev.w3.org/html5/spec/
-function outline(node) {
-
-    var current_outline_target = null;
-    var current_section = null;
-    var stack = [];
-
-    function visit(enter, node) {
-        var exit = !enter;
-
-        // When exiting an element, if that element is the element at
-        // the top of the stack
-        if (exit && stack.last() == node) {
-            // Pop that element from the stack.
-            stack.pop();
-        }
-
-        // If the top of the stack is a heading content element or an
-        // element with a hidden attribute
-        else if (is_heading(stack.last()) || ignore(stack.last())) {
-            // Do nothing.
-        }
-
-        // When entering an element with a hidden attribute
-        else if (enter && ignore(node)) {
-            // Push the element being entered onto the stack.
-            stack.push(node);
-        }
-
-        // When entering a sectioning content element or a sectioning root element
-        else if (enter && is_section(node) || is_root(node)) {
-            // If current outline target is not null, and the current
-            // section has no heading, create an implied heading and
-            // let that be the heading for the current section.
-            if ((current_outline_target != null) && "I Give up") {
-                var implied_heading = "(paragraph)";
-            }
-
-            // If current outline target is not null, push current
-            // outline target onto the stack.
-
-            // Let current outline target be the element that is being
-            // entered.
-
-            // Let current section be a newly created section for the
-            // current outline target element.
-
-            // Associate current outline target with current section.
-
-            // Let there be a new outline for the new current outline
-            // target, initialized with just the new current section
-            // as the only section in the outline.
-        }
-
-        // When exiting a sectioning content element, if the stack is not empty
-        else if (exit && stack && is_section(node)) {
-            // If the current section has no heading, create an
-            // implied heading and let that be the heading for the
-            // current section.
-
-            // Pop the top element from the stack, and let the current
-            // outline target be that element.
-
-            // Let current section be the last section in the outline of the
-            // current outline target element.
-
-            // Append the outline of the sectioning content element
-            // being exited to the current section. (This does not
-            // change which section is the last section in the
-            // outline.)
-        }
-
-        // When exiting a sectioning root element, if the stack is not empty
-        else if (exit && stack && is_root(node)){
-
-            // If the current section has no heading, create an
-            // implied heading and let that be the heading for the
-            // current section.
-
-            // Pop the top element from the stack, and let the current
-            // outline target be that element.
-
-            // Let current section be the last section in the outline
-            // of the current outline target element.
-
-            // Finding the deepest child: If current section has no
-            // child sections, stop these steps.
-
-            // Let current section be the last child section of the
-            // current current section.
-
-            // Go back to the substep labeled finding the deepest
-            // child.
-        }
-        
-        // When exiting a sectioning content element or a sectioning root element
-        if (exit && is_section(node) || is_root(node)) {
-
-            // Note: The current outline target is the element being exited,
-            // and it is the sectioning content element or a
-            // sectioning root element at the root of the subtree for
-            // which an outline is being generated.
-
-            // If the current section has no heading, create an
-            // implied heading and let that be the heading for the
-            // current section.
-
-            // Skip to the next step in the overall set of steps. (The walk is over.)
-        }
-
-        // When entering a heading content element
-        else if (enter && is_heading(node)) {
-
-            // If the current section has no heading, let the element
-            // being entered be the heading for the current section.
-
-            // Otherwise, if the element being entered has a rank
-            // equal to or higher than the heading of the last section
-            // of the outline of the current outline target, or if the
-            // heading of the last section of the outline of the
-            // current outline target is an implied heading, then
-            // create a new section and append it to the outline of
-            // the current outline target element, so that this new
-            // section is the new last section of that outline. Let
-            // current section be that new section. Let the element
-            // being entered be the new heading for the current
-            // section.
-
-            // Otherwise, run these substeps:
-
-            // Let candidate section be current section.
-
-            // Heading loop: If the element being entered has a rank
-            // lower than the rank of the heading of the candidate
-            // section, then create a new section, and append it to
-            // candidate section. (This does not change which section
-            // is the last section in the outline.) Let current
-            // section be this new section. Let the element being
-            // entered be the new heading for the current
-            // section. Abort these substeps.
-
-            // Let new candidate section be the section that contains
-            // candidate section in the outline of current outline target.
-
-            // Let candidate section be new candidate section.
-
-            // Return to the step labeled heading loop.
-
-            // Push the element being entered onto the stack. (This
-            // causes the algorithm to skip any descendants of the
-            // element.)
-        }
-    }
-}
-
-function walk(root, visit) {
-    var node = root;
-    start: while (node) {
-        visit(true, node);
-        if (node.firstElementChild) {
-            node = node.firstElementChild;
-            continue start;
-        }
-        while (node) {
-            visit(false, node);
-            if (node == root) {
-                node = null;
-            } else if (node.nextElementSibling) {
-                node = node.nextElementSibling;
-                continue start;
-            } else {
-                node = node.parentNode;
-            }
-        }
-    }
-}
-
-
-
-// returns the structure of the tree beneath node
-function create_structure(node) {
-    var outline;
-    var section = node.firstElementChild;
-    while (section) {
-        if (is_heading(child)) {
-            var entry;
-            entry.name = node.textContent;
-            entry.node = node;
-        }
-
-	if (section.childElementCount) {
-	    create_outline(section); // recurse!
-	}
-	var next_section = section.nextElementSibling;
-	section = next_section;
-    }
-}
-
-
-function is_heading(node) {
-    switch (node.nodeName) {
-    case "H1":
-    case "H2":
-    case "H3":
-    case "H4":
-    case "H5":
-    case "H6":
-        return true
-    default:
-        return false
-    }
-}
-
-
-
-
-
-
-function insert_note(e) {
-    console.log(e);
-    console.log('inserting note!');
-    document.execCommand("formatBlock", false, 'aside');
-}
-
-// returns an event handler that toggles the element
+// Returns an event handler that toggles the element
 function make_toggler(element) {
     var visible = false;
     element.style.display = 'none';
@@ -278,6 +47,292 @@ function make_toggler(element) {
 	}
     };
 };
+
+/*
+                document
+document        +--section
++--h1              +--h1
++--p      ===>     +--p
++--h2              +--section
++--p                  +--h2
+                      +--p
+*/
+
+// Walks the elements of a DOM depth-first, calling enter(el) and
+// exit(el) when entering/leaving each node. As a premature
+// optimization, if enter(el) returns false it will not descend into the
+// subtree.
+function racewalk(root, enter, exit) {
+    var node = root;
+    start: while (node) {
+        if (enter(node) && node.firstElementChild) {
+            // descend!
+            node = node.firstElementChild;
+            continue start;
+        }
+        while (node) {
+            exit(node);
+            if (node == root) {
+                return;
+            } else if (node.nextElementSibling) {
+                // descend again!
+                node = node.nextElementSibling;
+                continue start;
+            } else {
+                node = node.parentNode;
+            }
+        }
+    }
+}
+
+// A customized implementation of the HTML5 outlining algorithm
+// described by http://dev.w3.org/html5/spec/ Also very much inspired
+// by https://github.com/hoyois/html5outliner (thanks Marc!)
+function HTMLOutline(root) {
+    
+    // BEGIN OUTLINE ALGORITHM
+    // STEP 1
+    var currentOutlinee = null; // element whose outline is being created
+    // STEP 2
+    var currentSection = null; // current section
+    
+    // STEP 3
+    // Minimal stack object
+    var stack = {"lastIndex": -1};
+    stack.isEmpty = function() {
+        return stack.lastIndex === -1;
+    };
+    stack.push = function(e) {
+        stack[++stack.lastIndex] = e;
+        stack.top = e;
+    };
+    stack.pop = function() {
+        var e = stack.top;
+        delete stack[stack.lastIndex--];
+        stack.top = stack[stack.lastIndex];
+        return e;
+    };
+    
+    // STEP 4 (minus DOM walk which is at the end)
+    function enter(node) {
+        if(isElement(node)) {
+            if(!stack.isEmpty() && (isHeadingElement(stack.top) || isHidden(stack.top))) {
+                // Do nothing
+            } else if(isHidden(node)) {
+                stack.push(node);
+            } else if(isSectioningContentElement(node) || isSectioningRootElement(node)) {
+                // if(currentOutlinee !== null && currentSection.heading === null) {
+                // Create implied heading
+                // }
+                if(currentOutlinee !== null) stack.push(currentOutlinee);
+                currentOutlinee = node;
+                currentSection = new Section();
+                associateNodeWithSection(currentOutlinee, currentSection);
+                currentOutlinee.appendSection(currentSection);
+            } else if(currentOutlinee === null) {
+                // Do nothing
+            } else if(isHeadingElement(node)) {
+                if(currentSection.heading === null) currentSection.heading = node;
+                else if(currentOutlinee.lastSection.heading === null || node.rank >= currentOutlinee.lastSection.heading.rank) {
+                    currentSection = new Section();
+                    currentSection.heading = node;
+                    currentOutlinee.appendSection(currentSection);
+                } else {
+                    var candidateSection = currentSection;
+                    do {
+                        if(node.rank < candidateSection.heading.rank) {
+                            currentSection = new Section();
+                            currentSection.heading = node;
+                            candidateSection.appendChild(currentSection);
+                            break;
+                        }
+                        var newCandidate = candidateSection.parentSection;
+                        candidateSection = newCandidate;
+                    } while(true);
+                }
+                stack.push(node);
+            } // else {
+            // Do nothing
+            // }
+        }
+    }
+    
+    function exit(node) {
+        if(isElement(node)) {
+            if(!stack.isEmpty() && node === stack.top) stack.pop();
+            else if(!stack.isEmpty() && (isHeadingElement(stack.top) || isHidden(stack.top))) {
+                // Do nothing
+            } else if(!stack.isEmpty() && isSectioningContentElement(node)) {
+                // if(currentSection.heading === null) {
+                // Create implied heading
+                // }
+                currentOutlinee = stack.pop();
+                currentSection = currentOutlinee.lastSection;
+                for(var i = 0; i < node.sectionList.length; i++) {
+                    currentSection.appendChild(node.sectionList[i]);
+                }
+            } else if(!stack.isEmpty() && isSectioningRootElement(node)) {
+                // if(currentSection.heading === null) {
+                // Create implied heading
+                // }
+                currentOutlinee = stack.pop();
+                currentSection = currentOutlinee.lastSection;
+                while(currentSection.childSections.length > 0) {
+                    currentSection = currentSection.lastChild;
+                }
+            } else if(isSectioningContentElement(node) || isSectioningRootElement(node)) {
+                // if(currentSection.heading === null) {
+                // Create implied heading
+                // }
+                // The algorith says to end the walk here, but that's assuming root is a sectioning element
+                // Instead we reset the algorithm for subsequent top-level sectioning elements
+                currentOutlinee = null;
+                currentSection = null;
+            } // else {
+            // Do nothing
+            // }
+        }
+        if(node.associatedSection === null && currentSection !== null) associateNodeWithSection(node, currentSection);
+    }
+    
+    // STEP 5
+    // The heading associated to node is node.associatedSection.heading, if any
+    // END OUTLINE ALGORITHM
+    
+    // Now we must make the necessary definitions for the above to make sense
+    function associateNodeWithSection(node, section) {
+        section.associatedNodes.push(node);
+        node.associatedSection = section;
+    }
+    
+    function isElement(node) {
+        return node.nodeType === 1;
+    }
+    
+    function isHidden(node) {
+        return node.hidden;
+    }
+    
+    function isSectioningContentElement(node) {
+        return node.sectionType === 1;
+    }
+    
+    function isSectioningRootElement(node) {
+        return node.sectionType === 2;
+    }
+    
+    function isHeadingElement(node) {
+        return node.rank !== undefined;
+    }
+    
+    function extend(node) {
+        if(node.nodeType === 1) {
+            switch(node.nodeName.toLowerCase()) {
+            case "blockquote": case "body": case "details": case "fieldset": case "figure": case "td":
+                extendSectioningRootElement(node);
+                break;
+            case "article": case "aside": case "nav": case "section":
+                extendSectioningContentElement(node);
+                break;
+            case "h1": case "h2": case "h3": case "h4": case "h5": case "h6":
+                extendHeadingElement(node);
+                break;
+            case "hgroup":
+                extendHeadingGroupElement(node);
+                break;
+            default:
+                extendNode(node);
+            }
+        } else extendNode(node);
+    }
+    
+    function extendNode(node) {
+        node.associatedSection = null;
+    }
+    
+    function extendSectioningElement(node) {
+        extendNode(node);
+        node.sectionList = [];
+        node.firstSection = null;
+        node.lastSection = null;
+        
+        node.appendSection = function(section) {
+            this.sectionList.push(section);
+            if(this.firstSection === null) this.firstSection = section;
+            this.lastSection = section;
+        };
+    }
+    
+    function extendSectioningContentElement(node) {
+        extendSectioningElement(node);
+        node.sectionType = 1;
+    }
+    
+    function extendSectioningRootElement(node) {
+        extendSectioningElement(node);
+        node.sectionType = 2;
+    }
+    
+    function extendHeadingContentElement(node) {
+        extendNode(node);
+        Object.defineProperty(node, "depth", {"get": function() {
+            var section = node.associatedSection;
+            var depth = 1;
+            if(section !== null) {
+                while(section = section.parentSection) ++depth;
+            }
+            return depth;
+        }, "configurable": true, "enumerable": true});
+    }
+    
+    function extendHeadingElement(node) {
+        extendHeadingContentElement(node);
+        node.rank = -parseInt(node.nodeName.charAt(1));
+        node.text = node.textContent;
+    }
+    
+    function extendHeadingGroupElement(node) {
+        extendHeadingContentElement(node);
+        
+        for(var i = 1; i <= 6; i++) {
+            var h = node.getElementsByTagName("h" + i);
+            if(h.length > 0) {
+                node.rank = -i;
+                node.text = h[0].textContent;
+                break;
+            }
+        }
+        
+        if(node.rank === undefined) {
+            node.rank = -1;
+            node.text = "";
+        }
+    }
+    
+    // Walk the DOM subtree of root
+    var node = root;
+    start: while(node) {
+        extend(node);
+        enter(node);
+        if(node.firstChild) {
+            node = node.firstChild;
+            continue start;
+        }
+        while(node) {
+            exit(node);
+            if(node === root) break start;
+            if(node.nextSibling) {
+                node = node.nextSibling;
+                continue start;
+            }
+            node = node.parentNode;
+        }
+    }
+}
+
+
+
+// THE FOLLOWING ARE BAD AND HAVE TO GO
 
 function update_outline() {
     var old_outline = $('nav ol');
@@ -355,11 +410,3 @@ function make_entry(node) {
     return li;
 }
 
-// poor man's jquery...
-function $(query) {
-    return document.querySelector(query);
-}
-
-function $$(query) {
-    return document.querySelectorAll(query);
-}
